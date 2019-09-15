4719872a46154c86ec68980a8296d0451991929b
ankita <ankitakumari650@gmail.com>
2019-09-15 15:17:59 +0530

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            for(int i=0;i<a;i++)
+            {
+                if (i == 3)
+                    break;
+                Console.WriteLine("inside break");
+            }
+            Console.WriteLine("outside break");
+            Console.ReadLine();
+        }
+    }
+}
